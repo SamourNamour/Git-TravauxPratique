e2d58e02c4e07e40c867fb667f90c889e7e17da9
SamourNamour <b.benahmida@gmail.com>
2017-09-21 10:05:34 +0100

    init login message

---
+++ test.py
@@ -0,0 +1,41 @@
+import math 
+
+def Montalab(x):
+    print("Montalab function is called")
+    return 1
+	
+def BrowzeList(list):
+	for x in list:
+		print (x)
+	return 1;
+
+#'
+
+myList = []
+
+myList.append(7.0)
+myList.append("delta")
+myList.append(isinstance(myList[1],str))
+myList.append(568)
+myList.append(10.254)
+myList.append("Apolo")
+myList.append("test string + ")
+
+# parcourir la list est imprimer ses elements sur l ecran en appellant la fonction BrowzeList
+
+# BrowzeList(myList)
+
+i = 10
+x = 8.2
+y = "Montasir"
+
+print (" note = %.5f , Name =%s, mode %X" %(x,y,i))
+
+
+
+
+
+
+
+
+
